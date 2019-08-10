031e20020932420b0f49f084643b6a3d2612b2fa
Michael Ira Krufky <mkrufky@gmail.com>
2019-08-10 18:31:04 -0400

    more refactoring match3 endpoint to be async

--- routes.go
+++ routes.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -334,60 +335,72 @@ func routes(ctx context.Context, e *echo.Echo) {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("pixel parsing for task1 failed: %s", err.Error()))
 		}
 
+		pass0 := 0
+		fail0 := 0
+		pass1 := 0
+		fail1 := 0
+		pass2 := 0
+		fail2 := 0
+
+		var wg sync.WaitGroup
+		wg.Add(3)
+
+		go func() {
+			defer wg.Done()
+			for x, a := range pixels0 {
+				for y, b := range a {
+					if math.Abs(float64(b.R-pixels1[x][y].R)) < float64(v) &&
+						math.Abs(float64(b.G-pixels1[x][y].G)) < float64(v) &&
+						math.Abs(float64(b.B-pixels1[x][y].B)) < float64(v) &&
+						math.Abs(float64(b.A-pixels1[x][y].A)) < float64(v) {
+						pass0 = pass0 + 1
+					} else {
+						fail0 = fail0 + 1
+					}
+				}
+			}
+		}()
+
 		pixels2, err := getPixels(img2)
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, fmt.Sprintf("pixel parsing for task2 failed: %s", err.Error()))
 		}
 
-		pass0 := 0
-		fail0 := 0
-		for x, a := range pixels0 {
-			for y, b := range a {
-				if math.Abs(float64(b.R-pixels1[x][y].R)) < float64(v) &&
-					math.Abs(float64(b.G-pixels1[x][y].G)) < float64(v) &&
-					math.Abs(float64(b.B-pixels1[x][y].B)) < float64(v) &&
-					math.Abs(float64(b.A-pixels1[x][y].A)) < float64(v) {
-					pass0 = pass0 + 1
-				} else {
-					fail0 = fail0 + 1
+		go func() {
+			defer wg.Done()
+			for x, a := range pixels1 {
+				for y, b := range a {
+					if math.Abs(float64(b.R-pixels2[x][y].R)) < float64(v) &&
+						math.Abs(float64(b.G-pixels2[x][y].G)) < float64(v) &&
+						math.Abs(float64(b.B-pixels2[x][y].B)) < float64(v) &&
+						math.Abs(float64(b.A-pixels2[x][y].A)) < float64(v) {
+						pass1 = pass1 + 1
+					} else {
+						fail1 = fail1 + 1
+					}
 				}
 			}
-		}
+		}()
 
+		go func() {
+			defer wg.Done()
+			for x, a := range pixels2 {
+				for y, b := range a {
+					if math.Abs(float64(b.R-pixels0[x][y].R)) < float64(v) &&
+						math.Abs(float64(b.G-pixels0[x][y].G)) < float64(v) &&
+						math.Abs(float64(b.B-pixels0[x][y].B)) < float64(v) &&
+						math.Abs(float64(b.A-pixels0[x][y].A)) < float64(v) {
+						pass2 = pass2 + 1
+					} else {
+						fail2 = fail2 + 1
+					}
+				}
+			}
+		}()
+
+		wg.Wait()
 		blob0 := fmt.Sprintf("s0 & s1: %v%% match\n", 100*float64(pass0)/float64(pass0+fail0))
-
-		pass1 := 0
-		fail1 := 0
-		for x, a := range pixels1 {
-			for y, b := range a {
-				if math.Abs(float64(b.R-pixels2[x][y].R)) < float64(v) &&
-					math.Abs(float64(b.G-pixels2[x][y].G)) < float64(v) &&
-					math.Abs(float64(b.B-pixels2[x][y].B)) < float64(v) &&
-					math.Abs(float64(b.A-pixels2[x][y].A)) < float64(v) {
-					pass1 = pass1 + 1
-				} else {
-					fail1 = fail1 + 1
-				}
-			}
-		}
-
 		blob1 := fmt.Sprintf("s1 & s2: %v%% match\n", 100*float64(pass1)/float64(pass1+fail1))
-
-		pass2 := 0
-		fail2 := 0
-		for x, a := range pixels2 {
-			for y, b := range a {
-				if math.Abs(float64(b.R-pixels0[x][y].R)) < float64(v) &&
-					math.Abs(float64(b.G-pixels0[x][y].G)) < float64(v) &&
-					math.Abs(float64(b.B-pixels0[x][y].B)) < float64(v) &&
-					math.Abs(float64(b.A-pixels0[x][y].A)) < float64(v) {
-					pass2 = pass2 + 1
-				} else {
-					fail2 = fail2 + 1
-				}
-			}
-		}
-
 		blob2 := fmt.Sprintf("s2 & s0: %v%% match\n", 100*float64(pass2)/float64(pass2+fail2))
 
 		// if fail0 > fail1 {
